8b6c81f4600ddb4d84e77fd35677ed7122831970
Evgeniy Golodnykh <evgenferrum@gmail.com>
2024-03-09 14:28:19 +0300

    add merch, ambassadors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
 
     'api',
     'users',
+    'ambassadors',
+    'merch',
 ]
 
 MIDDLEWARE = [
@@ -150,7 +152,7 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
